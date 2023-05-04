985f3f13f72d9a283138371d4f25b937d050dfcc
Dzmitry Tsybulski <dzmitrytsybulski@gmail.com>
2023-05-04 11:07:19 +0300

    Correction 6 Stage 2 Module #1. Task #1. Build Tools. Gradle

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -6,14 +6,14 @@ import java.util.regex.Pattern;
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
         try {
-            Pattern pattern = Pattern.compile("0.+");
+            Pattern pattern = Pattern.compile("^0");
             Matcher matcher = pattern.matcher(str);
             if (matcher.find()) {
                 return false;
             }
             System.out.println(Double.parseDouble(str));
             return Double.parseDouble(str) > 0;
-        } catch (NumberFormatException e) {
+        } catch (NumberFormatException | NullPointerException e) {
             return false;
         }
     }
